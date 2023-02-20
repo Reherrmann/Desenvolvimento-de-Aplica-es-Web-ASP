1abca9cfef4db2e523853a2b02f2c7e9322500d5
Reherrmann <94018742+Reherrmann@users.noreply.github.com>
2023-02-20 19:49:47 -0300

    Add files via upload

---
+++ Am.Web/Controllers/AmigoController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amigos.services.interfaces;
+using Base.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Am.Web.Controllers
+{
+    public class AmigoController : Controller
+    {
+        private readonly IAmigosService _amigoService;
+
+        public AmigoController(IAmigosService amigosService)
+        {
+            _amigoService = amigosService;
+        }
+
+        public IActionResult Index()
+        {
+             // var amigos = amigosService.GetAmigos();
+
+         return View(_amigoService.GetAmigos());
+            //  return View(amigos);
+        }
+    }
+}
---
+++ Am.Web/Controllers/ListController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Base.Domain.Entities;
+using Amigos.services.interfaces;
+
+
+
+namespace Am.Web.Controllers
+{
+    public class ListController : Controller
+    {
+        private readonly IAmigosService _amigoService;
+        public ListController(IAmigosService amigosService)
+        {
+            _amigoService = amigosService;
+        }
+
+        [Route("/List/ListaDeAmigos")]
+        public IActionResult ListaDeAmigos()
+        {
+
+            var listaDeAmigos = _amigoService.GetAmigos();
+            return View(listaDeAmigos);
+        }
+
+        [HttpPost]
+        [Route("list/selecionar")]
+        public IActionResult Selecionar()
+        {
+            return View();
+        }
+
+
+    }
+}
